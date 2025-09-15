6bd4e10e2fd69a03b33d290b22eee6b7b20a6f8a
«Anastasia <a.chertilina104@gmail.com>
2025-09-15 23:13:00 +0300

    Stop tracking folder and add to .gitignore

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anastasia/dev/project7/rpn_calculator/tests
-# Build directory: /Users/anastasia/dev/project7/rpn_calculator/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[RPN_Tests]=] "/Users/anastasia/dev/project7/rpn_calculator/build/tests/rpn_tests")
-set_tests_properties([=[RPN_Tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/anastasia/dev/project7/rpn_calculator/tests/CMakeLists.txt;13;add_test;/Users/anastasia/dev/project7/rpn_calculator/tests/CMakeLists.txt;0;")
